17a10925e59b63061a125645c9cf6032898fa64b
Koushlendra Kumar <code.koushlendra@gmail.com>
2022-11-04 14:08:54 +0530

    Create navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <h1><span>KK's</span> CMS</h1>
+        <div>
+          <a href='k'>Create</a>
+          <a href='k'>Edit</a>
+          <a href='k'>Login</a>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
