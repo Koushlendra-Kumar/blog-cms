93c0b03b730ddffb8a52bb3a6752987dd7bf6f91
Koushlendra Kumar <code.koushlendra@gmail.com>
2022-11-04 14:20:04 +0530

    Create footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Form from './components/Form';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Form />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div>
+            &copy; 2022 KK's CMS
+        </div>
+    )
+}
+
+export default Footer;
